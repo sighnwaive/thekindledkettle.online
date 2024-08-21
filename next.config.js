773ed10463b3879c6efa33f1725606f147cbdef7
Dennise Bartlett <bartlett.dc.1@gmail.com>
2024-08-21 03:32:40 -0500

    Add BasePath

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/thekindledkettle.online",
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
